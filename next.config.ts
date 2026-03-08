377f0f155aeca30381deba916318a85396f18925
Yin Lijun <yinlijun7403@gmail.com>
2026-03-08 17:10:43 +0800

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/yinlijun-portfolio',
+  assetPrefix: '/yinlijun-portfolio',
 };
 
 export default nextConfig;
